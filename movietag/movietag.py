d379d358166a5a0a4e5dfa6669ac71c59bb32cd0
Mark Riedesel <mark@klowner.com>
2015-06-26 16:48:11 -0500

    added really crappy support for tv episodes

--- movietag/movietag.py
+++ movietag/movietag.py
@@ -1,19 +1,38 @@
-import os
-import omdb
 import argparse
-import subprocess
 import mimetypes
+import omdb
+import os
+import re
+import subprocess
+import tvdb_api
 
 def main():
     parser = argparse.ArgumentParser(description='Automatically apply metadata to movie files')
     parser.add_argument('file', type=argparse.FileType('rb', 0))
     parser.add_argument('-m', dest='movie', type=str, help='Movie title to search for')
+    parser.add_argument('-s', dest='series', type=str, help='TV series to search for')
+    parser.add_argument('-gt', dest='guesstv', action='store_true', help='Infer series based on filename')
 
     args = parser.parse_args()
 
     if (args.movie):
         print('Searching for movie: "%s"...' % args.movie)
         return movie_main(args)
+
+    if (args.guesstv):
+        print('Guessing television episode based on filename...')
+        return tv_main(args)
+
+def apply_meta(args, meta):
+    mimetype = mimetypes.guess_type(os.path.abspath(args.file.name))
+
+    if mimetype[0] in [
+            'video/x-m4v',
+            'video/mp4'
+            ] or os.path.splitext(args.file.name)[1] in ['.m4v', '.mp4']:
+        return apply_m4v_meta(args.file, meta)
+    else:
+        print('Unsupported file type: %s' % mimetype[0])
 
 
 def movie_main(args):
@@ -27,15 +46,54 @@ def movie_main(args):
             print('%s:\t%s' % (k,v))
 
         if choice('Apply?'):
-            mimetype = mimetypes.guess_type(os.path.abspath(args.file.name))
+            apply_meta(args, info);
 
-            if mimetype[0] in [
-                    'video/x-m4v',
-                    'video/mp4'
-                    ]:
-                return apply_m4v_meta(args.file, info)
-            else:
-                print('Unsupported file type: %s' % mimetype[0])
+
+def guess_tv_attributes(name):
+    name_re = re.compile(r'(.*)[sS](\d*)[eE](\d*)\.')
+    matches = name_re.match(name)
+
+    if matches:
+        matches = matches.groups()
+        name = matches[0].strip().replace('-', ' ').replace('_', ' ')
+        season = int(matches[1])
+        episode = int(matches[2])
+        return (name, season, episode)
+
+
+def tv_main(args):
+    t = tvdb_api.Tvdb()
+    name, season, ep = guess_tv_attributes(os.path.basename(args.file.name))
+
+    episode = t[name][season - 1][ep]
+
+    meta = get_tv_meta(episode)
+
+    if meta:
+        for k,v in meta.items():
+            print('%s:\t%s' % (k,v))
+
+        if args.guesstv or choice('Apply?'):
+            apply_meta(args, meta);
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def get_tv_meta(episode):
+    imdb = omdb.get(imdbid=episode['imdb_id'])
+
+    meta = AttrDict(
+        title  = episode['episodename'],
+        actors = imdb['actors'],
+        year   = imdb['year'],
+        plot   = episode['overview'],
+        genre  = imdb['genre'],
+    )
+
+    return meta
 
 
 def choice(message, default='y'):
